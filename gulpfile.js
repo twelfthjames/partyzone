8b569c43eccc9be0ce8fdb3c6f74eed406ad52c0
Joel Shaw <jshaw@rvadesignsolutions.com>
2017-07-06 22:13:29 -0400

    Add minification and base HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp'),
       gls = require('gulp-live-server'),
+      htmlmin = require('gulp-htmlmin'),
       sass = require('gulp-sass');
 
-gulp.task('styles', function(){
-    return gulp.src('app/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('build/css'));
+gulp.task('minify', function(){
+    return gulp.src('app/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('serve', function(){
     const server = gls.static('build', 3000);
+});
+
+gulp.task('styles', function(){
+    return gulp.src('app/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'))
 });
 
 gulp.task('default', ['styles', 'serve']);
