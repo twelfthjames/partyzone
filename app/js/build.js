d9afee66c47f09fa5712bed9069bcb878e826435
Joel Shaw <jshaw@rvadesignsolutions.com>
2017-08-02 20:17:01 -0400

    Create components stylesheet, Zone Overview component

--- app/js/build.js
+++ app/js/build.js
@@ -3,3 +3,13 @@ var partyZone = angular.module('partyZone', []);
 partyZone.controller('partyZoneController', function partyZoneController(){
    
 });
+
+partyZone.component('zoneOverview', {
+    templateUrl: 'templates/overview.html',
+    bindings: {
+        name: "<"
+    },
+    controller: function() {
+        console.log(this.name)
+    }
+});
