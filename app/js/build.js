7384bb26bb9ffc821618b14638299e37802c0525
Joel Shaw <jshaw@rvadesignsolutions.com>
2017-08-28 22:46:34 -0400

    Add 'all zones' section, including menu board and past zones, restyled zone bricks

--- app/js/build.js
+++ app/js/build.js
@@ -1,9 +1,7 @@
-var partyZone = angular.module('partyZone', ['ui.router']);
+var partyZone = angular.module('partyZone', []);
 
-partyZone.controller('partyZoneController', ['$scope', function($scope){
-    $scope.title = 'test';
-
-    var total = 5;
+partyZone.controller('partyZoneController', ['$scope', function($scope) {
+    $scope.city = 'Richmond';
 }]);
 
 partyZone.component('zoneOverview', {
@@ -11,27 +9,51 @@ partyZone.component('zoneOverview', {
     bindings: {
         name: "<"
     },
-    controller: function() {
-        console.log(this.name)
-    }
+    controller: 'zoneOverview'
 });
+
+partyZone.controller('zoneOverview', ['$scope', function($scope){
+    $scope.total = "2";
+}]);
+
+
+// function zoneOverview($scope){
+//     $scope.title = 'test';
+//     $scope.total = 5;
+// }
+
+
 
 partyZone.component('zonesAll', {
     templateUrl: 'templates/zones.html',
     bindings: {
         name: "<"
     },
-    controller: function(){
-        console.log(this.name)
-    }
+    controller: 'allZones'
 });
+
+partyZone.controller('allZones', ['$scope', function($scope) {
+    $scope.test = 'test';
+    $scope.zones = [
+        {name: 'The Friend Zone', venue: 'The Veil Brewing Co.', date: new Date('09/07/2017')},
+        {name: 'PZ Launch Party', venue: 'Kabana Rooftop Lounge', date: new Date('02/01/2018')}
+    ];
+
+    $scope.past = [
+      {name: 'Past Zone 1'}  
+    ];
+    $scope.noPast = 'Once you complete your first PZ, you\'ll be able to get a recap here!';
+}]);
+
 
 partyZone.component('zoneSettings', {
     template: 'settings',
     bindings: {
         name: "<"
     },
-    controller: function(){
-        console.log(this.name)
-    }
+    controller: 'zoneSettings'
 });
+
+partyZone.controller('zoneSettings', ['$scope', function($scope) {
+
+}]);
